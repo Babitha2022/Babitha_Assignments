35f9bfbf655eac1d2bb2fb97f1d4995261ec4564
Babitha2022 <119962555+Babitha2022@users.noreply.github.com>
2022-12-10 15:07:31 +0530

    Add files via upload

---
+++ selenium/week2/day1/AutomateLoginSalesForce.java
@@ -0,0 +1,38 @@
+package selenium.week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutomateLoginSalesForce {
+
+	public static void main(String[] args) 
+	{
+		//Open the Chrme browser
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		//Open the application
+		driver.get("https://login.salesforce.com/");
+		
+		//To locate the username
+		WebElement username = driver.findElement(By.id("username"));
+		
+		//To enter the username in text
+		username.sendKeys("hari.radhakrishnan@qeagle.com");
+		
+		//To locate the password 
+		WebElement pwd = driver.findElement(By.id("password"));
+		
+		//to enter the password in the text
+		pwd.sendKeys("Testleaf$321");
+		
+		//to locate Login button
+		WebElement Login = driver.findElement(By.name("Login"));
+		
+		//to click Log In button
+		Login.click();
+		
+	}
+
+}
